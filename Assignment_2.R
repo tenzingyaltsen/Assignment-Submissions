7aa169418c6e170e93065ef4d2dc45400799fea9
Ahmed Mokhtar <ahmed.mokhtar@mail.utoronto.ca>
2024-06-19 11:15:59 -0400

    Added a placeholder that is set to true, but changes to False if any of the conditions are met. Then, a check is made to see if the placeholder is still true. If it is, then the armstrong check is conducted.

--- Assignment_2.R
+++ Assignment_2.R
@@ -2,27 +2,34 @@
 
 number <- readline("Enter a natural number from 100 to 999: ")
 
-number <- as.numeric(number)
-#number <- suppressWarnings(as.numeric(number))
+number <- suppressWarnings(as.numeric(number))
+
+placeholder <- TRUE
 
 if (is.na(number)) {
   
   print("The value you have entered is not numeric. Source and try again.")
+  placeholder <- FALSE
 
 } else if (number < 100) {
   
   print(paste(number, "is less than 100. Try again with a number from 100 to 999"))
+  placeholder <- FALSE
 
 } else if (number > 999) {
   
   print(paste(number, "is more than 999. Try again with a number from 100 to 999"))
+  placeholder <- FALSE
 
 } 
 
-armstrong <- c(153, 370, 371, 407)
-
-if (number %in% armstrong) {
+if (placeholder == TRUE) {
+  
+  armstrong <- c(153, 370, 371, 407)
+  
+  if (number %in% armstrong) {
   print(paste(number, "is an Armstrong number. Good job!"))
-} else {
+  } else {
   print(paste(number, "is not Armstrong number. Try again!"))
+  }
 }
