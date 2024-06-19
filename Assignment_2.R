cc055688b65b2a48155e16aea38d0e2172b740fe
Ahmed Mokhtar <ahmed.mokhtar@mail.utoronto.ca>
2024-06-19 10:28:58 -0400

    Added a prompt to suppress warnings. Changed the wording of the initial readline. This change makes it easier for the reader to understand what mistake they committed if they did.

--- Assignment_2.R
+++ Assignment_2.R
@@ -1,14 +1,20 @@
 # Assignment 2 - Ahmed Mokhtar
 
-number <- readline("Enter a three digit positive number: ")
+number <- readline("Enter a number from 100 to 999: ")
 
 number <- as.numeric(number)
+#number <- suppressWarnings(as.numeric(number))
 
 if (is.na(number)) {
   
   print("The value you have entered is not numeric. Source and try again.")
-} else if (is.numeric(number)) {
+} else if (number < 100) {
   
-  print(paste("Thank you! The number you entered is", number)) 
+  print(paste(number, "is less than 100. Try again with a number from 100 to 999"))
+  
+} else if (number > 999) {
+  
+  print(paste(number, "is more than 999. Try again with a number from 100 to 999"))
+  
 }
 
