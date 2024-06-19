4aaff36c9c57bfd43a7c2e047a732683a8cbbbbc
Ahmed Mokhtar <ahmed.mokhtar@mail.utoronto.ca>
2024-06-19 10:03:36 -0400

    The prompts have been reversed. The functions now work as intended.

--- Assignment_2.R
+++ Assignment_2.R
@@ -4,11 +4,11 @@ number <- readline("Enter a three digit positive number: ")
 
 number <- as.numeric(number)
 
-if (is.numeric(number)) {
-  
-  print(paste("Thank you! The number you entered is", number)) 
-  
-} else if (is.na(number)) {
+if (is.na(number)) {
   
   print("The value you have entered is not numeric. Source and try again.")
+} else if (is.numeric(number)) {
+  
+  print(paste("Thank you! The number you entered is", number)) 
 }
+
