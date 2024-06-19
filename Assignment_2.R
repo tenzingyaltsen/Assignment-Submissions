7df4e276fb949233b7e6818dba43a688e3f852be
Ahmed Mokhtar <ahmed.mokhtar@mail.utoronto.ca>
2024-06-19 11:04:58 -0400

    Added the armstrong check. Now trying to find a way to not get to the armstrong check if it meets one of the conditions before that.

--- Assignment_2.R
+++ Assignment_2.R
@@ -1,6 +1,6 @@
 # Assignment 2 - Ahmed Mokhtar
 
-number <- readline("Enter a number from 100 to 999: ")
+number <- readline("Enter a natural number from 100 to 999: ")
 
 number <- as.numeric(number)
 #number <- suppressWarnings(as.numeric(number))
@@ -8,13 +8,21 @@ number <- as.numeric(number)
 if (is.na(number)) {
   
   print("The value you have entered is not numeric. Source and try again.")
+
 } else if (number < 100) {
   
   print(paste(number, "is less than 100. Try again with a number from 100 to 999"))
-  
+
 } else if (number > 999) {
   
   print(paste(number, "is more than 999. Try again with a number from 100 to 999"))
-  
-}
 
+} 
+
+armstrong <- c(153, 370, 371, 407)
+
+if (number %in% armstrong) {
+  print(paste(number, "is an Armstrong number. Good job!"))
+} else {
+  print(paste(number, "is not Armstrong number. Try again!"))
+}
