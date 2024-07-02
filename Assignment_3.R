af986ae78da5a71b38450451875f058b7ab73c41
Ahmed Mokhtar <ahmed.mokhtar@mail.utoronto.ca>
2024-07-02 00:35:31 -0400

    List and sample

--- Assignment_3.R
+++ Assignment_3.R
@@ -1,9 +1,12 @@
 # Assignment 3 - Ahmed Mokhtar
 
+#' The text file containing the list of words was created. I then used the 
+#' readLines function to read the file, and assigned it to "list".
 
+list <- readLines("hangman.txt")
 
-
-
+#' The "sample" function allows R to randomly select one word from "list".
+sample(list, 1)
 
 
 
